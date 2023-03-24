ddc25732b5b26de8c4489db78de40d637790e4c2
AaronB <aaronbesson@gmail.com>
2023-03-24 15:17:06 -0400

    push

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,8 +2,6 @@
  * Change the index and namespace to your own
  */
 
-const PINECONE_INDEX_NAME = 'langchainpdftest';
-
-const PINECONE_NAME_SPACE = 'tt-2023-budget'; //namespace is optional for your vectors
-
+const PINECONE_INDEX_NAME = 'ttbudget2023';
+const PINECONE_NAME_SPACE = 'ttbudget2023'; //namespace is optional for your vectors
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
