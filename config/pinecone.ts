79d8d6d77a73f8c2be8c47b9aea13fefac907666
AaronB <aaronbesson@gmail.com>
2023-03-25 11:24:03 -0400

    push

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -1,7 +1,6 @@
 /**
  * Change the index and namespace to your own
  */
-
 const PINECONE_INDEX_NAME = 'ttbudget2023';
 const PINECONE_NAME_SPACE = 'CPL2022Stats'; //namespace is optional for your vectors
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
